7e7de8cac2d0c0b63f29ca98ff5ecd44360c69dd
Andrew Nease <andrew.nease.code@gmail.com>
2020-08-26 12:59:48 -0400

    fix adapter endpoint

--- src/adapters/star-wars-adapter.js
+++ src/adapters/star-wars-adapter.js
@@ -3,10 +3,11 @@ import axios from 'axios'
 export default {
   hitStartWarsAPI: function () {
 
-  return axios.post(`https://swapi.co/api/people/1`)
+    return axios.get(`http://swapi.dev/api/people/1/`)
     .then( (response) => {
+      console.log('response');
       return { starWarsCharacter: response.data.name }
-    })
+    }).catch((err) => console.log(err))
 
   }
 }
